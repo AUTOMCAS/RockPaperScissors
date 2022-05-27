c18059b2f63bbeab053bcd7d6bebf0fbd9d30193
cas <caston88@gmail.com>
2022-05-28 03:39:54 +1200

    Add function for playRound, win conditions and score tracker

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const rps =["Rock", "Paper", "Scissors"];
+const playerSelection = "Rock"
+computerSelection = " "
+let playerScore = 0
+let computerScore = 0
 
 
 function computerPlay() {
     const random = Math.floor(Math.random() * 3);
-    console.log(rps[random], random);
+    computerSelection = rps[random];
 }
 
+function playRound() {
+    computerPlay()
+    if (playerSelection === computerSelection) {
+    console.log("Draw!");
+    } else if ((playerSelection==="Rock" && computerSelection==="Scissors") 
+    || (playerSelection==="Paper" && computerSelection==="Rock")
+    || (playerSelection==="Scissors" && computerSelection==="Paper")) {
+        console.log("Player wins!");
+        playerScore = ++playerScore;
+        console.log(playerScore)
+            if(playerScore === 5) {
+                console.log("Player wins!!!")}
+    } else {
+    console.log("Computer wins!");
+    computerScore = ++computerScore;
+    console.log(computerScore)
+        if(computerScore === 5) {
+        console.log("Computer wins!!!")}
+    }
+}
