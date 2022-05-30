2d391c00625cdaea2508b428a41c3d3f2baf207d
cas <caston88@gmail.com>
2022-05-30 22:13:08 +1200

    Remove score tally from playRound in prep to create game function

--- main.js
+++ main.js
@@ -1,32 +1,74 @@
 const rps =["Rock", "Paper", "Scissors"];
-const playerSelection = "Rock"
-computerSelection = " "
+
+
 let playerScore = 0
 let computerScore = 0
 
 
+
+
 function computerPlay() {
     const random = Math.floor(Math.random() * 3);
-    computerSelection = rps[random];
+    return rps[random];
+
 }
 
-function playRound() {
-    computerPlay()
+const playerSelection = "Rock"
+
+
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
     console.log("Draw!");
     } else if ((playerSelection==="Rock" && computerSelection==="Scissors") 
     || (playerSelection==="Paper" && computerSelection==="Rock")
     || (playerSelection==="Scissors" && computerSelection==="Paper")) {
-        console.log("Player wins!");
-        playerScore = ++playerScore;
-        console.log(playerScore)
-            if(playerScore === 5) {
-                console.log("Player wins!!!")}
+        console.log("Player +1");
+
     } else {
-    console.log("Computer wins!");
-    computerScore = ++computerScore;
-    console.log(computerScore)
-        if(computerScore === 5) {
-        console.log("Computer wins!!!")}
+    console.log("Computer +1");
     }
 }
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    playRound(playerSelection);
+}
+
+
+
+/*
+function func1(func_param){
+    func_param();
+    console.log(func_param());
+
+}
+
+function func2(){
+    console.log("Okay");
+
+}
+
+func1(func2)
+
+
+
+function pass1(value) {
+    return ("Hello " + value);
+}
+
+function pass2() {
+    return (" Howdy!");
+}
+
+function receive_pass(funcA, funcB) {
+    console.log(funcA("world!")+funcB());
+
+}
+
+receive_pass(pass1, pass2);
+*/
+
