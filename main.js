1be3ac4d2ecbf5a70bd9e693c3cb694bbddcf225
cas <caston88@gmail.com>
2022-05-30 22:32:59 +1200

    Add game function to run rounds until player or computer reach 5, then reset

--- main.js
+++ main.js
@@ -13,9 +13,6 @@ function computerPlay() {
 
 }
 
-const playerSelection = "Rock"
-
-
 
 function playRound(playerSelection) {
     const computerSelection = computerPlay();
@@ -25,20 +22,31 @@ function playRound(playerSelection) {
     || (playerSelection==="Paper" && computerSelection==="Rock")
     || (playerSelection==="Scissors" && computerSelection==="Paper")) {
         console.log("Player +1");
+        playerScore = ++playerScore;
 
     } else {
     console.log("Computer +1");
+    computerScore = ++computerScore;
     }
 }
 
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    const playerSelection = prompt ("What do you choose?")
     playRound(playerSelection);
+    
+        if (playerScore === 5) {
+            console.log("Player wins!");
+            computerScore = 0
+            playerScore = 0
+        } else if (computerScore===5) { 
+            console.log("Computer wins!");
+            computerScore = 0
+            playerScore = 0
+        }
+ 
+
 }
-
-
 
 /*
 function func1(func_param){
